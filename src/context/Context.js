ff43bfc000197289aa321bcceb53c1d3440b17f8
Roger Wells <rsw359@gmail.com>
2023-01-17 13:55:46 +0900

    registration and login bug being worked on

---
+++ src/context/Context.js
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from "react";
+import LoginReducer from "./Reducer";
+const INITIAL_STATE = {
+  user: null,
+  isFetching: false,
+  error: false,
+};
+
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(LoginReducer, INITIAL_STATE);
+
+  return (
+    <Context.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,50 @@
 import "./login.css";
 import { Link } from "react-router-dom";
+import { useRef, useContext } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+  console.log(isFetching);
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label className="inputLabel">Email</label>
-        <input type="text" className="loginInput" placeholder="email" />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label className="inputLabel">Username</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter Username"
+          ref={userRef}
+        />
 
         <label className="inputLabel">Password</label>
-        <input type="Password" className="loginInput" placeholder="password" />
-        <button className="loginButton">Login</button>
+        <input
+          type="Password"
+          className="loginInput"
+          placeholder="Enter Password"
+          ref={passwordRef}
+        />
+        <button className="loginButton" type="submit">
+          Login
+        </button>
       </form>
       <button className="registerButton">
         <Link className="link" to="/register">
